40520eb6d8dc3b608bf5c87b34d6412168fedd33
JS Soluções e Serviços <49538838+jssolucoes-e-servicos@users.noreply.github.com>
2025-10-10 14:59:06 -0300

    update apoadores

--- src/constants/landing-page.ts
+++ src/constants/landing-page.ts
@@ -21,11 +21,85 @@ export const LANDING_PAGE_MEDIA = {
   ],
   patrocinios: [
     {
-      url: '/assets/images/dogao/patrocinio/armarinho.png',
+      url: '/assets/images/dogao/patrocinio/agroverde.jpeg',
       description: 'Nossos Apoiadores',
     },
     {
-      url: '/assets/images/dogao/patrocinio/caravagio.png',
+      url: '/assets/images/dogao/patrocinio/amu-leto.jpeg',
+      description: 'Nossos Apoiadores',
+    },
+
+
+    {
+      url: '/assets/images/dogao/patrocinio/amu-pizzas.jpeg',
+      description: 'Nossos Apoiadores',
+    },
+    {
+      url: '/assets/images/dogao/patrocinio/armarinho.jpeg',
+      description: 'Nossos Apoiadores',
+    },
+    {
+      url: '/assets/images/dogao/patrocinio/caravagio.jpeg',
+      description: 'Nossos Apoiadores',
+    },
+    {
+      url: '/assets/images/dogao/patrocinio/colombo.jpeg',
+      description: 'Nossos Apoiadores',
+    },
+    {
+      url: '/assets/images/dogao/patrocinio/espaco-da-moda.jpeg',
+      description: 'Nossos Apoiadores',
+    },
+    {
+      url: '/assets/images/dogao/patrocinio/happy-acai.jpeg',
+      description: 'Nossos Apoiadores',
+    },
+    {
+      url: '/assets/images/dogao/patrocinio/jck.jpeg',
+      description: 'Nossos Apoiadores',
+    },
+    {
+      url: '/assets/images/dogao/patrocinio/keppler.jpeg',
+      description: 'Nossos Apoiadores',
+    },
+    {
+      url: '/assets/images/dogao/patrocinio/lab47.jpeg',
+      description: 'Nossos Apoiadores',
+    },
+    {
+      url: '/assets/images/dogao/patrocinio/lhc.jpeg',
+      description: 'Nossos Apoiadores',
+    },
+    {
+      url: '/assets/images/dogao/patrocinio/madkasa.jpeg',
+      description: 'Nossos Apoiadores',
+    },
+    {
+      url: '/assets/images/dogao/patrocinio/mns.jpeg',
+      description: 'Nossos Apoiadores',
+    },
+    {
+      url: '/assets/images/dogao/patrocinio/papel-novo.jpeg',
+      description: 'Nossos Apoiadores',
+    },
+    {
+      url: '/assets/images/dogao/patrocinio/porto.jpeg',
+      description: 'Nossos Apoiadores',
+    },
+    {
+      url: '/assets/images/dogao/patrocinio/silk-print.jpeg',
+      description: 'Nossos Apoiadores',
+    },
+    {
+      url: '/assets/images/dogao/patrocinio/sos-chaveiros.jpeg',
+      description: 'Nossos Apoiadores',
+    },
+    {
+      url: '/assets/images/dogao/patrocinio/sul-pao.jpeg',
+      description: 'Nossos Apoiadores',
+    },
+    {
+      url: '/assets/images/dogao/patrocinio/rossato.jpeg',
       description: 'Nossos Apoiadores',
     },
   ],
